89d8ae8799e1adc4fb2b470596d197dc38377fa5
Пользователь <saidzalolovsaidaziz@gmail.com>
2024-11-14 20:23:57 +0500

    second_commit

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -6,3 +6,6 @@ class Data(models.Model):
     email = models.EmailField()
     password = models.CharField(max_length=100)
     gender = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.first_name
